711ce36e3cd90e906322e0bb28e075fdd3e1347c
Christian Mayrhofer <neugier4000@yahoo.de>
2018-02-13 09:58:32 +0100

    Alles fein

---
+++ Lab01/BooksSample/BookSample/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSample.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string ISBN { get; set; }
+
+        public override string ToString() => Title;
+    }
+}
---
+++ Lab01/BooksSample/BooksSample/Model/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksSample.Model
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string Isbn { get; set; }
+    }
+}
