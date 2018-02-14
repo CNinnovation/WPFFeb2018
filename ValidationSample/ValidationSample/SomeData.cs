343b723fd4f4f09f598f4bea1d2c8fda77ad40ff
Christian Nagel <christian@christiannagel.com>
2018-02-14 16:04:47 +0100

    validation

---
+++ ValidationSample/ValidationSample/SomeData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationSample
+{
+    public class SomeData : IDataErrorInfo, INotifyDataErrorInfo
+    {
+        private int _x;
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (columnName == nameof(Y))
+                {
+                    if (Y > 25)
+                    {
+                        return "Wrong value in y";
+                    }
+                }
+                return null;
+            }
+        }
+
+        public int X
+        {
+            get => _x;
+            set
+            {
+                if (value > 25) throw new Exception("bad bad value");
+                _x = value;
+            }
+        }
+
+        public int Y { get; set; }
+
+        public string Error => throw new NotImplementedException();
+    }
+}
