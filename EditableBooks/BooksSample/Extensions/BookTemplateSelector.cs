607beffad9170aa3bdc2ca9c94220e183b52595b
Christian Nagel <christian@christiannagel.com>
2018-02-14 12:57:50 +0100

    edit books

---
+++ EditableBooks/BooksSample/Extensions/BookTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using BooksSample.Models;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BooksSample.Extensions
+{
+    public class BookTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultBookTemplate { get; set; }
+        public DataTemplate WroxBookTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            DataTemplate selectedTemplate = null;
+            if (item is Book book)
+            {
+                switch (book.Publisher)
+                {
+                    case "Wrox Press":
+                        selectedTemplate = WroxBookTemplate;
+                        break;
+                    default:
+                        selectedTemplate = DefaultBookTemplate;
+                        break;
+                }
+            }
+
+            return selectedTemplate;
+        }
+    }
+}
---
+++ EditableBooks/BooksSample/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using BooksSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BooksSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Book> _books = new ObservableCollection<Book>(new BookFactory().GetBooks());
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _books;
+        }
+
+        private void OnAddBook(object sender, RoutedEventArgs e)
+        {
+            _books.Add(new Book("Professional C# 8", "Wrox Press", "3478234", "Christian nagel"));
+        }
+
+        private void OnChangeBook(object sender, RoutedEventArgs e)
+        {
+            var book = _books.Single(b => b.Title == "Professional C# 6");
+            book.Title = "Professional C# 7";
+        }
+    }
+}
