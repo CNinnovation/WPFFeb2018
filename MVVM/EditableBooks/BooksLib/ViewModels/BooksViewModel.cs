30bae92f6ca952bb752fe923a99a98f5350e6182
Christian Nagel <christian@christiannagel.com>
2018-02-15 13:24:27 +0100

    MVVM sample

---
+++ MVVM/EditableBooks/BooksLib/ViewModels/BooksViewModel.cs
@@ -0,0 +1,47 @@
+﻿using BooksLib.Models;
+using BooksLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using TheBestMVVMFrameworkInTown.Core;
+using TheBestMVVMFrameworkInTown.MVVM;
+
+namespace BooksLib.ViewModels
+{
+    public class BooksViewModel : ViewModelBase
+    {
+        private readonly IBooksService _booksService;
+        private readonly IDialogService _dialogService;
+        private readonly ObservableCollection<Book> _books;
+        public BooksViewModel(IBooksService booksService, IDialogService dialogService)
+        {
+            _booksService = booksService;
+            _dialogService = dialogService;
+
+            _books = new ObservableCollection<Book>(_booksService.GetBooks());
+
+            AddBooksCommand = new DelegateCommand(OnAddBook, CanAddBook);
+            ShowMessageCommand = new DelegateCommand(OnShowMessage);
+        }
+
+        private async void OnShowMessage()
+        {
+            await _dialogService.ShowMessageAsync("Hello from the View-Model");
+        }
+
+        public IEnumerable<Book> Books => _books;
+
+        public ICommand AddBooksCommand { get; }
+
+        public ICommand ShowMessageCommand { get; }
+
+        public void OnAddBook()
+        {
+            _books.Add(new Book("Professional C# 8", "Wrox Press", "3478234", "Christian nagel"));
+        }
+
+        public bool CanAddBook() => true;
+    }
+}
---
+++ MVVM/EditableBooks/BooksSample/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using BooksLib.Services;
+using BooksLib.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BooksSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // ViewModel = new BooksViewModel(new BooksService());
+            ViewModel = (Application.Current as App).Container.GetService<BooksViewModel>();
+
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public BooksViewModel ViewModel { get; }
+
+
+        //private void OnChangeBook(object sender, RoutedEventArgs e)
+        //{
+        //    var book = _books.Single(b => b.Title == "Professional C# 6");
+        //    book.Title = "Professional C# 7";
+        //}
+
+        //private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+
+        //}
+    }
+}
